b141f05bfdf16cdfe05cb1ccd43b003f7805ea59
Evgeny Samarin <ey.samarin@gmail.com>
2023-02-17 02:52:05 +0300

    add base stylish templates

--- server/app/app.py
+++ server/app/app.py
@@ -1,14 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+main_menu = [
+    {"name": "Sign-In", "url": "index"},
+    {"name": "About", "url": "about"},
+]
 
-@app.route('/')
+
 @app.route('/index')
+@app.route('/auth', methods=["POST"])
+@app.route('/')
 def index():
-    return "<h1>Hello, World!</h1>"
+    """
+    rander main page
+    """
+    # debug logging
+    print(url_for('index'))
+
+    if request.method == "POST":
+        print(request.form)
+
+    return render_template(
+        'index.html',
+        title="Log-in",
+        header="Authorization",
+        menu=main_menu,
+    )
+
+
+@app.route('/about')
+def about():
+    """
+    render about page
+    """
+    # debug logging
+    print(url_for('about'))
+    return render_template(
+        'about.html',
+        title="About us",
+        header="About site",
+        menu=main_menu,
+    )
 
 
 if __name__ == '__main__':
