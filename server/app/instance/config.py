4d80dfcdcc4a58fea6144e0fe9827ee28865e130
Evgeny Samarin <ey.samarin@gmail.com>
2023-02-17 21:59:30 +0300

    unhide configuration file

---
+++ server/app/instance/config.py
@@ -0,0 +1,9 @@
+"""
+Flask configuration. Currently, stubs and fakes
+
+IN CASE OF PUBLICATION TO A PUBLIC REPOSITORY, THIS FILE MUST BE HIDDEN
+"""
+
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = '86548a1a-aee2-11ed-9522-ee0f590d2416'
